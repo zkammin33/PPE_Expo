431b70b5f9e03f71cd614e532d99d97d44eb846b
Zach Kammin <zachkammin@gmail.com>
2021-08-09 16:07:44 -0500

    Replaced Dialog Box with individual 'Create' and 'Cancel' Buttons Implemented input check for create button

--- New_User_Class.py
+++ New_User_Class.py
@@ -14,11 +14,39 @@ class CreateWindow(QtWidgets.QWidget):
         self.window_pos_y = (self.screen_height - self.height()) / 3
         self.move(self.window_pos_x, self.window_pos_y)
 
-        self.create_user_ui.confirm_new_user_btnbx.accepted.connect(self.ok_button_clicked)
-        self.create_user_ui.confirm_new_user_btnbx.rejected.connect(self.cancel_button_clicked)
+        self.create_user_ui.create_button.clicked.connect(self.ok_button_clicked)
+        self.create_user_ui.cancel_button.clicked.connect(self.cancel_button_clicked)
+        self.create_user_ui.first_name_input.textChanged.connect(self.on_text_change)
+        self.create_user_ui.last_name_input.textChanged.connect(self.on_text_change)
+        self.create_user_ui.middle_name_input.textChanged.connect(self.on_text_change)
+        self.create_user_ui.new_email_input.textChanged.connect(self.on_text_change)
+        self.create_user_ui.new_pass_input.textChanged.connect(self.on_text_change)
+        self.create_user_ui.confirm_pass_input.textChanged.connect(self.on_text_change)
+        self.create_user_ui.phone_num_input.textChanged.connect(self.on_text_change)
 
     def ok_button_clicked(self):
-        print("Hello")
+        print(self.create_user_ui.first_name_input.text())
+        print(self.create_user_ui.last_name_input.text())
+        print(self.create_user_ui.middle_name_input.text())
+        print(self.create_user_ui.new_email_input.text())
+        print(self.create_user_ui.new_pass_input.text())
+        print(self.create_user_ui.confirm_pass_input.text())
+        print(self.create_user_ui.phone_num_input.text())
+        self.close()
 
     def cancel_button_clicked(self):
         self.close()
+
+    def on_text_change(self):
+        if (self.create_user_ui.first_name_input.text() != "" and
+                self.create_user_ui.last_name_input.text() != "" and
+                self.create_user_ui.middle_name_input.text() != "" and
+                self.create_user_ui.new_email_input.text() != "" and
+                self.create_user_ui.new_pass_input.text() != "" and
+                self.create_user_ui.confirm_pass_input.text() != "" and
+                self.create_user_ui.phone_num_input.text() != ""):
+
+            self.create_user_ui.create_button.setEnabled(True)
+
+        else:
+            self.create_user_ui.create_button.setEnabled(False)
