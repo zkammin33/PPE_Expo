a05df7ec792ed9969a35786f610d030e3a07172f
Zach Kammin <zachkammin@gmail.com>
2021-09-29 19:30:08 -0500

    Create new user pushes data entered onto server

--- New_User_Class.py
+++ New_User_Class.py
@@ -1,7 +1,9 @@
+import mysql.connector
 from PySide6 import QtWidgets
 import pyautogui
 import New_User
-import Login_Class
+from mysql.connector import connect, Error
+from getpass import getpass
 
 
 class CreateWindow(QtWidgets.QWidget):
@@ -24,14 +26,55 @@ class CreateWindow(QtWidgets.QWidget):
         self.create_user_ui.confirm_pass_input.textChanged.connect(self.on_text_change)
         self.create_user_ui.phone_num_input.textChanged.connect(self.on_text_change)
 
+        # try:
+        #     with connect(
+        #         host="localhost",
+        #         #user=input("Enter Username: "),
+        #         #password=getpass("Enter Password: "),
+        #         user="ZPKammin",
+        #         password="Coebaseball1!",
+        #         database="ZKammin"
+        #     ) as connection:
+        #         print(connection)
+        #         with connection.cursor() as cursor:
+        #             cursor.execute("DESCRIBE user_info")
+        #             for x in cursor:
+        #                 print(x)
+        #         cursor.close()
+        #         connection.close()
+        # except Error as e:
+        #     print(e)
+        # print("DONE")
+
+
     def ok_button_clicked(self):
-        print(self.create_user_ui.first_name_input.text())
-        print(self.create_user_ui.last_name_input.text())
-        print(self.create_user_ui.middle_name_input.text())
-        print(self.create_user_ui.new_email_input.text())
-        print(self.create_user_ui.new_pass_input.text())
-        print(self.create_user_ui.confirm_pass_input.text())
-        print(self.create_user_ui.phone_num_input.text())
+        try:
+            cnx = mysql.connector.connect(host='localhost', user="ZPKammin",
+                                          password="Coebaseball1!", database="ZKammin")
+            mycursor = cnx.cursor()
+            mycursor.execute("""INSERT INTO user_info (first_name, middle_initial,
+                             last_name, email, phone_number, password) VALUES
+                             (%s,%s,%s,%s,%s,%s)""",
+                                (self.create_user_ui.first_name_input.text(),
+                                 self.create_user_ui.middle_name_input.text(),
+                                 self.create_user_ui.last_name_input.text(),
+                                 self.create_user_ui.new_email_input.text(),
+                                 self.create_user_ui.phone_num_input.text(),
+                                 self.create_user_ui.new_pass_input.text()))
+            cnx.commit()
+
+            mycursor.execute(f"SELECT * FROM user_info WHERE first_name = '{self.create_user_ui.first_name_input.text()}'")
+            row = mycursor.fetchall()
+            member_id = row[0][0]
+            mycursor.execute("INSERT INTO turnout_gear (Owner_ID, Helmet, Coat) VALUES "
+                             "(%s,%s,%s)",
+                             (17, 1112311, 123114))
+            cnx.commit()
+        except Error as e:
+            print(e)
+
+
+        cnx.close()
         self.close()
 
     def cancel_button_clicked(self):
